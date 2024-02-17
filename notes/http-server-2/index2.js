5ae3baff01479a91f8957f1b2fd94ed28d132282
KushagraaWadhwa <21f3002930@ds.study.iitm.ac.in>
2024-02-17 23:07:07 +0530

    regular change

--- notes/http-server-2/index2.js
+++ notes/http-server-2/index2.js
@@ -1,8 +1,24 @@
 const express = require('express')
-const app = express()
+const app = express();  
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/route-handler', function(req,res){
+  //headers, body, query parameters
+  //do machine learning model
+  res.json({
+    name:"Kushagra Wadhwa",
+    age: 21
+
+  })
+})
+
+
+app.get('/', function(req, res){
+  let a=0;
+  console.log("Request has been reached");
+  for (let i=0; i<100;i++){
+    a=a+1;   
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                   i<100000000000
   res.send('Hello World!')
 })
 
