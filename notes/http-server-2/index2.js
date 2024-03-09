f8b77c78a1218358d81769204282d8c56e13da2e
KushagraaWadhwa <21f3002930@ds.study.iitm.ac.in>
2024-03-09 12:53:16 +0530

    index2 updatee

--- notes/http-server-2/index2.js
+++ notes/http-server-2/index2.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();  
 const port = 3000
+const bodyParser=require("body-parser")    
 
 app.get('/route-handler', function(req,res){
   //headers, body, query parameters
@@ -21,6 +22,14 @@ app.get('/', function(req, res){
 }                                                                                                                                                                                                                                                                                                                                                                                                                                                   i<100000000000
   res.send('Hello World!')
 })
+
+app.post('/backend-api/conversation', (req,res) => {
+  const message=req.body.message;
+  res.json({
+    output:"2+2=4"
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
